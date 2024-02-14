50803b5c338a3bc657b45d2fd1321b291571a673
Sergiu BOCANCIA <sbocanci@paul-f3Ar5s1.clusters.42paris.fr>
2024-02-14 15:21:57 +0100

    Removed the SocketListen class, moved the socket logic to Server class directly.

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,27 +1,28 @@
 #include "Server.hpp"
 
 Server::Server() : 
+	_socket_fd(-1),
 	_port(PORT),
 	_serverName(SERVER_NAME),
 	_serverPassword(SERVER_PASSWORD) {
 
 	std::cout << MAGENTA << "\tServer constructor called" << RESET << std::endl;
 
-	std::cout << YELLOW << "\tinitializing server socket.." << RESET << std::endl;
+	std::cout << YELLOW << "\tinitializing server socket [ listen ].." << RESET << std::endl;
 	initServerSocket();
 }
 
 Server::~Server() {
 
 	std::cout << RED << "\tServer destructor called" << RESET << std::endl;
+
+	// This might leak when server is interrupted with ctrl+c. Need to handle this in signal handler
+	close(_socket_fd);
 }
 
-/*
-** This GETTER returns the `_socket_fd` value from `SocketListen` class.
-*/
 int		Server::getServerFd() const {
 
-	return _serverSocket.getSocketFd();
+	return (_socket_fd);
 }
 
 std::string	Server::getServerName() const {
@@ -31,9 +32,55 @@ std::string	Server::getServerName() const {
 
 void	Server::initServerSocket() {
 
-	_serverSocket.initSocket(AF_INET, SOCK_STREAM, PROTOCOL, _port, INADDR_ANY);
-	_serverSocket.connectToNetwork();
-	_serverSocket.startListenToNetwork(BACKLOG);
+	initSocket();
+	connectToNetwork();
+	startListenToNetwork(BACKLOG);
+}
+
+void	Server::initSocket() {
+
+	// Defining address structure
+	_address.sin_family = AF_INET;
+	_address.sin_port = htons(_port);
+	_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	_socket_fd = socket(AF_INET, SOCK_STREAM, PROTOCOL); // AF_INET = IPv4, SOCK_STREAM = TCP, 0 = IP
+
+	if (_socket_fd < 0) {
+		perror("In Server::initSocket()");
+		exit(EXIT_FAILURE);
+	}
+
+	std::cout << GREEN << "\t[+] Socket created successfully. _socket_fd = " << this->_socket_fd << RESET << std::endl; 
+}
+
+void	Server::connectToNetwork() {
+
+	int	bindStatus = -1;
+	int opt = 1;
+
+	// `SO_REUSEADDR` will allow to use socket right after the program exits without waiting for the operating system to clean up the socket
+	if (setsockopt(_socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+		std::cerr << RED << "\t[-] Error setting socket options.. setsockopt() failed." << RESET << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	bindStatus = bind(_socket_fd, (struct sockaddr *)&_address, sizeof(_address));
+
+	if (bindStatus < 0) {
+		perror("In Server::connectToNetwork()");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	Server::startListenToNetwork(int backlog) {
+
+	int	listenReturn = listen(_socket_fd, backlog);
+
+	if (listenReturn < 0) {
+		perror("In Server::startListenToNetwork()");
+		exit(EXIT_FAILURE);
+	}
 }
 
 /*
@@ -43,7 +90,7 @@ void	Server::initServerSocket() {
 void	Server::printServerData() const {
 
 	std::cout << YELLOW << "[!] Server DATA:" << RESET << std::endl;
-	std::cout << YELLOW << "\tServer fd: " << _serverSocket.getSocketFd() << RESET << std::endl;
+	std::cout << YELLOW << "\tServer fd: " << getServerFd() << RESET << std::endl;
 	std::cout << YELLOW << "\tServer port: " << _port << RESET << std::endl;
 	std::cout << YELLOW << "\tServer name: " << _serverName << RESET << std::endl;
 	std::cout << YELLOW << "\tServer password: " << _serverPassword << RESET << std::endl;
--- Server/Server.hpp
+++ Server/Server.hpp
@@ -1,7 +1,24 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
-#include "../Socket/SocketListen.hpp"
+#include <iostream>
+#include <string>
+#include <cstdio> // perror()
+#include <cstdlib> // exit()
+#include <unistd.h> // close()
+#include <sys/socket.h> // socket(), bind(), listen(), accept()
+#include <netinet/in.h> // sockaddr_in, INADDR_ANY
+#include <sys/types.h>  // for u_long
+#include <sys/select.h> // for fd_set
+
+// SOME COLORS FOR MAKING THE LIFE BRIGTHER !!!
+#define RED		"\033[1;31m"
+#define GREEN	"\033[1;32m"
+#define YELLOW	"\033[1;33m"
+#define BLUE	"\033[1;34m"
+#define MAGENTA	"\033[1;35m"
+#define CYAN	"\033[1;36m"
+#define RESET	"\033[0m"
 
 /* THE FOLLOWING DEFINITIONS SHALL BE CHANGED ..*/
 #define PROTOCOL	0
@@ -16,12 +33,14 @@
 class Server {
 
 	private:
-		//int										_serverFd;
-		int										_port;
-		std::string								_serverName;
-		std::string								_serverPassword;
 
-		SocketListen							_serverSocket;
+		struct sockaddr_in	_address;
+		int					_socket_fd; // this is the file descriptor returned by `socket()`
+		int					_port;
+		std::string			_serverName;
+		std::string			_serverPassword;
+
+		// SocketListen							_serverSocket;
 
 	public:
 		Server();
@@ -31,6 +50,11 @@ class Server {
 		std::string			getServerName() const;
 
 		void				initServerSocket();
+
+		// void	initSocket(int domain, int service, int protocol, int port, u_long interface);
+		void	initSocket();
+		void	connectToNetwork(); // bind():
+		void	startListenToNetwork(int backlog); // listen():
 
 		/* DEBUG */
 		void				printServerData() const;
