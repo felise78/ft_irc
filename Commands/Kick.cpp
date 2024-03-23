0670d89b3c3b07667e0d59462aa646632e7b6fc2
Helene MOTTU <hemottu@paul-f3Ar4s2.clusters.42paris.fr>
2024-03-23 20:41:59 +0100

    KICK works git diff! / noticed pb tho with the modif I made on /nick command :s

--- Commands/Kick.cpp
+++ Commands/Kick.cpp
@@ -4,63 +4,54 @@ void	CommandHandler::handleKICK()
 {
  	std::cout << YELLOW << "KICK command received.." << RESET << std::endl;
 
-	// format de la commande : /KICK #channel nickname
+	// format de la commande : /KICK #channel nickname reason
 
 	std::vector<std::string> params = split(commandsFromClient["params"], " ");
-	if (params.begin() + 1 != params.end() && params.begin() + 2 != params.end())
-	{
-		/*DEBUG*/
-		int i = 0;
-		vector<string>::iterator it = params.begin();
-		for ( ; it != params.end(); ++it)
-		{
-			std::cout << i << std::endl;
-			i++;
-		}
-		server.setBroadcast(ERR_TOOMANYTARGETS(server.hostname, *(params.end() - 1)), user.getFd());
-		// user.responseBuffer = ERR_TOOMANYTARGETS(*(params.end() - 1));
-		return;
-	}
-	else if (params.begin() + 1 == params.end())
+
+	std::string channelName = *params.begin();
+	// deja gere par irssi but keeping it for netcat ?
+	if (params.begin() + 1 == params.end() || ( params.begin() + 2 == params.end() && channelName[0] == '#'))
 	{
 		server.setBroadcast(ERR_NEEDMOREPARAMS(server.hostname, commandsFromClient["command"]), user.getFd());
-		// user.responseBuffer = ERR_NEEDMOREPARAMS(commandsFromClient["command"]);
 		return;
 	}
-	std::string channelName = parse_channelName(*params.begin());
-	if (channelName.empty() == true || server.channelMap.find(channelName) == server.channelMap.end())
+	if (server.channelMap.find(channelName) == server.channelMap.end())
 	{
 		server.setBroadcast(ERR_NOSUCHCHANNEL(server.hostname, user.getNickName(), channelName), user.getFd());
-		// user.responseBuffer = ERR_NOSUCHCHANNEL(channelName);
 		return; 
 	}
 	if (server.channelMap[channelName]._users.find(user.getNickName()) == server.channelMap[channelName]._users.end())
 	{
 		server.setBroadcast(ERR_USERNOTINCHANNEL(server.hostname, user.getNickName(), channelName), user.getFd());
-		// user.responseBuffer = ERR_USERNOTINCHANNEL(user.getNickName(), channelName);
-		return;
-	}
-	if (server.channelMap[channelName].isOp(user.getNickName()) == false)
-	{
-		server.setBroadcast(ERR_CHANOPRIVSNEEDED(server.hostname, channelName), user.getFd());
-		// user.responseBuffer = ERR_CHANOPRIVSNEEDED(channelName);
 		return;
 	}
 	std::string nickname = *(params.begin() + 1);
 	if (server.usersMap.find(server.getFdbyNickName(nickname)) == server.usersMap.end())
 	{
 		server.setBroadcast(ERR_NOSUCHNICK(server.hostname, user.getNickName(), nickname), user.getFd());
-		// user.responseBuffer = ERR_NOSUCHNICK(nickname);
+		return;
+	}
+	if (server.channelMap[channelName].isOp(user.getNickName()) == false)
+	{
+		server.setBroadcast(ERR_CHANOPRIVSNEEDED(server.hostname, user.getNickName(), channelName), user.getFd());
 		return;
 	}
 	if (server.channelMap[channelName]._users.find(nickname) == server.channelMap[channelName]._users.end())
 	{
 		server.setBroadcast(ERR_USERNOTINCHANNEL(server.hostname, nickname, channelName), user.getFd());
-		// user.responseBuffer = ERR_USERNOTINCHANNEL(nickname, channelName); 
 		return;
 	}
-	server.channelMap[channelName].getUser(nickname).getChannel(channelName).removeUser(nickname);
-	server.channelMap[channelName].getUser(nickname).removeChannel(channelName);
+	std::string reason;
+	for (int i = 2; (params.begin() + i) != params.end() ; ++i)
+	{
+		std::string word = *(params.begin() + i);
+		reason += word;
+		if (params.begin() + i + 1 != params.end())
+			reason += " ";
+	}
 	server.channelMap[channelName].removeUser(nickname);
-	server.setBroadcast(RPL_KICK(server.hostname, user.getNickName(), channelName, nickname, ""), user.getFd());
+	server.usersMap[server.getFdbyNickName(nickname)].removeChannel(channelName);
+	server.setBroadcast(RPL_KICK(user.getPrefix(), channelName, nickname, reason), user.getFd());
+	server.setBroadcast(RPL_KICK(user.getPrefix(), channelName, nickname, reason), server.getFdbyNickName(nickname));
+	server.setBroadcast(channelName, user.getNickName(), RPL_KICK(user.getPrefix(), channelName, nickname, reason));
 }
