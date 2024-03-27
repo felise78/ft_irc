2a51ad17a0859e9c44641e175d1e041d40d0302b
Pia CHATTERJEE <pichatte@paul-f3Ar3s1.clusters.42paris.fr>
2024-03-27 12:57:29 +0100

    Parsed arguments to main

--- main.cpp
+++ main.cpp
@@ -11,7 +11,17 @@ int	main(int argc, char **argv) {
 	}
 	int port = atoi(argv[1]); // Probably needs more error handling here. 
 							// Which ports are valid? Eg. Tried 0, and server launches, but connections from irssi impossible.
+	if (port < 1 || port > 65000)
+	{
+		std::cerr << RED << "Invalid port" << RESET << "\n";
+		return 1;
+	}
 	std::string password = argv[2];
+	if (password.empty())
+	{
+		std::cerr << RED << "Invalid password" << RESET << "\n";
+		return 1;
+	}
 	ServerManager	serverManager(port, password);
 
 	return 0;
